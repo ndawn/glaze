58b5c415468a9d64136990e871e9c94bd49de137
Max <burmistrovm@live.ru>
2020-03-31 19:51:44 +0300

    Configured JWT authentication instead of standard token authentication

--- glaze/router.py
+++ glaze/router.py
@@ -2,7 +2,7 @@ from accounts.views import UserViewSet
 from image.views import ImageViewSet, UploadView, ImageRetrieveView
 
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 
 
@@ -11,7 +11,8 @@ router.register(r'users', UserViewSet, basename='users')
 router.register(r'images', ImageViewSet, basename='images')
 
 urlpatterns = [
-    path('authorize/', obtain_auth_token, name='authorize'),
+    path('authorize/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('authorize/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('upload/', UploadView.as_view(), name='upload'),
     path('<pk>/', ImageRetrieveView.as_view(), name='image_view'),
     # path('image/<id>/<transformations>/', TransformView.as_view(), name='image_transform'),
